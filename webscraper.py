c05a7494555b74c122b35ad85ebe0b3bad959a46
Son Pham <78460399+kypro34@users.noreply.github.com>
2022-07-07 16:28:13 +0700

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-url = 'https://www.youtube.com/c/KalleHallden/videos' 
+url = 'https://www.youtube.com/c/*Example*/videos' 
 browser = webdriver.Chrome()
 browser.get(url)
 browser.find_element_by_xpath('//*[@id="thumbnail"]').click()
